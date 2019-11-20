acf8a99ee546b08943df4cb78c51e77208de6533
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-20 18:57:10 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,6 +5,7 @@
 class EmailAddressParser
 
   attr_accessor :email_addresses, :csv
+<<<<<<< HEAD
   def initialize(email_addresses)
     @email_addresses = email_addresses
   end
@@ -25,4 +26,27 @@ if email_address_array.length > 1
   end
   email_address_array
 end
+=======
+  def initialize(email_addresses,csv=nil)
+    @email_addresses = email_addresses
+    @csv = csv
+  end
+
+  def parse
+    comma_parse
+    space_parse
+end
+def comma_parse
+  if email_addresses.include?(",")
+  email_address_array=  @email_addresses.split(",")
+else nil
+end
+def space_parse
+  if email_addresses.include?(" ")
+   email_address_array= @email_addresses.split(" ")
+ else nil
+ end
+end
+  end
+>>>>>>> 26e3e545ac6a1bc49004f63e3b2ce4c5a485069d
 end
